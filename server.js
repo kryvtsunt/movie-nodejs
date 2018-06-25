8e650fe21f4cc141bee3ff191f2728da381c001f
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-06-25 03:41:46 -0400

    heroku

--- server.js
+++ server.js
@@ -16,20 +16,9 @@ var app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin",
-//         "https://tk-movie-angular.herokuapp.com");
+//         "http://localhost:4200");
 //     res.header("Access-Control-Allow-Headers",
 //         "Origin, X-Requested-With, Content-Type, Accept");
 //     res.header("Access-Control-Allow-Methods",
@@ -37,6 +26,17 @@ app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Credentials", "true");
 //     next();
 // });
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "https://tk-movie-angular.herokuapp.com");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 
 var session = require('express-session')
